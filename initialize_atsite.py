a910e66b5bf482bad794f2b0d6abd7ff9b7279ba
KazukiShiotani <shiotani@nagoya-u.ac.jp>
2019-08-24 09:59:49 +0000

    add a init script at site

---
+++ initialize_atsite.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+Initialize XFFTS @ NANTEN2
+--------------------------
+
+BE : 1
+Bandwidth : 2000 [MHz]
+"""
+
+import udp_client
+
+client = udp_client.udp_client()
+client.open()
+client.stop()
+
+client.set_synctime(100000)              # synctime : 100 ms
+client.set_usedsections([1]*16)          # using board : 16
+for i in range(1, 17):
+    client.set_board_bandwidth(i, 2000)  # bandwidth : 2000 MHz
+client.configure()                       # apply settings
+client.caladc()                          # re-calibration for all ADCs
+client.start()                           # start measurement
+
+
+# History
+# -------
+# 2017/12/21 : written by T.Inaba
